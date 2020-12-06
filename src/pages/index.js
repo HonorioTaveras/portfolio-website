787f70349366ff781f0d2f18e7268adda7171e6f
Honorio <htaveras90@gmail.com>
2020-12-05 17:16:40 -0700

    added banner component and styles, deleted excess image.js file, added a bunch of placeholder pngs to project for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import '../styles/index.scss';
 
 // Import Components
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
 
 const IndexPage = () => (
   <div>
     <Header />
+    <Banner />
   </div>
 )
 
